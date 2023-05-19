73d3db5a2b4505677855239dde0d17fcebc8e600
swaraj-ssk <swarajkalbande123@gmail.com>
2023-05-20 02:57:37 +0530

    first JDBC project

--- src/demoJDBC/demo.java
+++ src/demoJDBC/demo.java
@@ -1,5 +1,28 @@
 package demoJDBC;
 
-public class demo {
+import java.sql.*;
 
+
+public class demo {
+	public static void main(String srgs[]) throws Exception {
+		
+		String url = "jdbc:mysql://localhost:3306/abc";
+		String uname = "root";
+		String pass = "Swaraj@2001";
+		
+		String query = "Select name from student where rollno=2";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection con = DriverManager.getConnection(url, uname, pass);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query); // this will give whole table
+		rs.next();
+		String name = rs.getString("name");
+		
+		System.out.println(name);
+		
+		st.close();
+		con.close();
+	};
 }
